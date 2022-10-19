086d09db24fe43b8a06169b4aeecab7799516133
B. Vich <tolo@tovilo.es>
2022-10-19 21:00:23 +0200

    Update manage.py

--- manage.py
+++ manage.py
@@ -11,13 +11,13 @@ app.config['DEBUG'] = True # Ensure debugger will load.
 def create_tables():
     "Create relational database tables."
     db.create_all()
-    paciente=Pacientes(nombre="Bartolomé", apellidos="Vich Lozano", dni="40123123E", telefono1="971000000", telefono2="871000000")
+    paciente=Pacientes(nombre="BME", apellidos="V", dni="40123123E", telefono1="971000000", telefono2="871000000")
     db.session.add(paciente)
-    paciente2=Pacientes(nombre="Javier", apellidos="Cuenca Valdomero", dni="44123123X", telefono1="971111111", telefono2="871111111")
+    paciente2=Pacientes(nombre="Javier", apellidos="Cuenc", dni="44123123X", telefono1="971111111", telefono2="871111111")
     db.session.add(paciente2)
-    paciente3=Pacientes(nombre="Pepe", apellidos="Gotera Tubería", dni="45123123R", telefono1="971222222", telefono2="871222222")
+    paciente3=Pacientes(nombre="Pepe", apellidos="Gotera", dni="45123123R", telefono1="971222222", telefono2="871222222")
     db.session.add(paciente3)
-    paciente4=Pacientes(nombre="Juan Carlos", apellidos="Albertí Gómez", dni="46123123T", telefono1="9713333333", telefono2="8713333333")
+    paciente4=Pacientes(nombre="Juan Carlos", apellidos="Gómez", dni="46123123T", telefono1="9713333333", telefono2="8713333333")
     db.session.add(paciente4)
     cita=Citas(fecha_creacion= datetime.datetime(2019, 4, 19, 0, 0), fecha_cita= datetime.datetime(2019, 4, 19, 0, 0), precio=100, finalizada=EstadoCita.PENDIENTE, facturada=False, paciente_id=1)
     db.session.add(cita)
