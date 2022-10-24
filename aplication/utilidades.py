86d25e048defcb566906accd1cfb293caa494e18
B. Vich <tolo@tovilo.es>
2022-10-24 21:58:02 +0200

    Update utilidades.py

--- aplication/utilidades.py
+++ aplication/utilidades.py
@@ -3,7 +3,6 @@ import string
 
 
 def lista_letras_mayusculas():
-    #lista_letras_mayusculas=["A", "B", "C", "D", "E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
     lista_letras_mayusculas=[]
     for l in string.ascii_uppercase:
         lista_letras_mayusculas.append(l)
